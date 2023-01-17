573c596ab06d7cd6210fc49e5ed26c4f559e727a
olatubora-felix <olatubora1996@gmail.com>
2023-01-17 04:20:13 +0100

    styling added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import { IBM_Plex_Serif } from '@next/font/google'
 
+const ibm = IBM_Plex_Serif({
+    weight: ['100', '200', '400', '500', '700'],
+    subsets: ['latin'],
+})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <main className={ibm.className}>
+            <Component {...pageProps} />;
+        </main>
+    )
 }
---
+++ pages/coffee-store/[id].jsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Details = () => {
+    const router = useRouter()
+    console.log(router)
+  return <div>
+  
+  </div>;
+}
+
+export default Details
