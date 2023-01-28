5d9fafaba31f0f267e2966cab813da90f4993cf0
olatubora-felix <olatubora1996@gmail.com>
2023-01-28 12:49:24 +0100

    saerch box added

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -8,22 +8,21 @@ import styles from './Card.module.css'
 const Card = (props) => {
     return (
         <Link href={props.href} className={styles.cardLink}>
-            <Fade direction="up">
-                <div className={cls('glass', styles.container)}>
-                    <div className={styles.cardHeaderWrapper}>
-                        <h2 className={styles.cardHeader}>{props.name}</h2>
-                    </div>
-                    <div className={styles.cardImageWrapper}>
-                        <Image
-                            className={styles.cardImage}
-                            src={props.imgUrl}
-                            width={300}
-                            height={160}
-                            alt={props.name}
-                        />
-                    </div>
+            <div className={cls('glass', styles.container)}>
+                <div className={styles.cardHeaderWrapper}>
+                    <h2 className={styles.cardHeader}>{props.name}</h2>
                 </div>
-            </Fade>
+
+                <div className={styles.cardImageWrapper}>
+                    <Image
+                        className={styles.cardImage}
+                        src={props.imgUrl}
+                        width={300}
+                        height={160}
+                        alt={props.name}
+                    />
+                </div>
+            </div>
         </Link>
     )
 }
